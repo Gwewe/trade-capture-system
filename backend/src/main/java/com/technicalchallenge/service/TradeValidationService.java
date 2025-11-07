1cfe28a55a33e829998848bc702a47c161413562
Gwewe <wedna.guirand@gmail.com>
2025-11-07 02:05:53 +0000

    enhancement(ValidateTradeBusinessRules) First interaction: added the ValidationResult class, and the TradeValidationService. Within the TradeValidationService, added the first method to validateTradeBusinessRules

--- backend/src/main/java/com/technicalchallenge/service/TradeValidationService.java
+++ backend/src/main/java/com/technicalchallenge/service/TradeValidationService.java
@@ -1,4 +1,36 @@
 package com.technicalchallenge.service;
 
+import com.technicalchallenge.dto.TradeDTO;
+import com.technicalchallenge.validation.ValidationResult;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.LocalDate;
+
+@Service
 public class TradeValidationService {
+    private final Clock clock;
+
+    public TradeValidationService(Clock clock) {
+        this.clock = clock;
+    }
+
+    public ValidationResult validateTradeBusinessRules(TradeDTO tradeDTO) {
+        ValidationResult validationResult = new ValidationResult();
+
+//        Maturity date cannot be before start date or trade date
+        if (tradeDTO.getTradeMaturityDate().isBefore(tradeDTO.getTradeStartDate()) || tradeDTO.getTradeMaturityDate().isBefore(tradeDTO.getTradeDate())) {
+            throw new RuntimeException("Maturity date cannot be before Start date or Trade date");
+        }
+//        Start date cannot be before tradeDate
+        if (tradeDTO.getTradeStartDate().isBefore(tradeDTO.getTradeDate())) {
+            throw new RuntimeException("Start date cannot be before Trade date");
+        }
+        //Trade date cannot be more than 30 days in the past
+        if (tradeDTO.getTradeDate().isBefore(LocalDate.now(clock).minusDays(30))) {
+            throw new RuntimeException("Trade date cannot be more than 30 days in the past");
+        }
+        validationResult.markAsValid();
+        return validationResult;
+    }
 }
--- backend/src/main/java/com/technicalchallenge/validation/ValidationResult.java
+++ backend/src/main/java/com/technicalchallenge/validation/ValidationResult.java
@@ -1,4 +1,43 @@
 package com.technicalchallenge.validation;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationResult {
+    private Boolean valid = null;
+    private final List<String> allErrors = new ArrayList<>();
+
+
+    public boolean isValid() {
+        return Boolean.TRUE.equals(valid);
+    }
+
+    public void addingErrorsToList(String error) {
+        valid = false;
+        allErrors.add(error);
+    }
+
+    public void markAsValid() {
+        if (allErrors.isEmpty()) {
+            valid = true;
+        }
+    }
+
+    public List<String> getAllErrors() {
+        return new ArrayList<>(allErrors);
+    }
+
+    @Override
+    public String toString() {
+        if (valid == null){
+            return "Validation are not performed yet";
+        } else if (valid) {
+            return "Validation passed check";
+        } else {
+            return "Validation did not passed the check. Error:" + allErrors;
+        }
+    }
+
 }
